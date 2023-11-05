962ae097e8d99056ed47de1950b0d0af301300fd
seheraksam <05seher@gmail.com>
2023-11-05 13:15:30 +0300

    Functions

--- My project (1)/Assets/Scenes/Functions.cs
+++ My project (1)/Assets/Scenes/Functions.cs
@@ -4,7 +4,26 @@ using UnityEngine;
 
 public class publicmethodlar : MonoBehaviour
 {
+    /* private void Awake()
+    {
+        Debug.Log("Ben awake ile geldim");
+    }
+    private void OnEnable()
+    {
+        Debug.Log("Ben OnEnable ile geldim"); 
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Ben Start ile geldim");
+    }*/
 
+    // güncelleme fonksiyonlarının sırası
+    private void FixedUpdate()
+    {
+        Debug.Log("Ben FixedUpdate ile sürekli geliyorum");
+        // 0.2
+    }
     //  public
     //  private
 
@@ -74,6 +93,14 @@ public class publicmethodlar : MonoBehaviour
             StopAllCoroutines(); // bütün hepsini durdurur
         }
     }
-
+    private void LateUpdate()
+    {
+        Debug.Log("Ben LateUpdate ile sürekli geliyorum");
+    }
+    /*
+        private void OnDisable()
+        {
+            Debug.Log("Ben OnDisable ile geldim");
+        }*/
 
 }
